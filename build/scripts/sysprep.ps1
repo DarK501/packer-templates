8b7590c12dc69c5a7f301fa8036c81e4bf020f12
DarK501 <dark_side500@hotmail.com>
2017-04-27 13:25:40 +0100

    Switch Sysprep to use vm mode clean up sysprep.ps1 reduce auto logon count

--- build/scripts/sysprep.ps1
+++ build/scripts/sysprep.ps1
@@ -1,4 +1,1 @@
-# remove sysprep key that stops sysprep at end of build
 Remove-ItemProperty -Path "HKLM:\System\Setup\Status\SysprepStatus" -Name GeneralizationState
-
-#Start-Process -FilePath "c:\windows\system32\sysprep\sysprep.exe" -ArgumentList "/generalize /oobe /unattend:c:\windows\system32\sysprep\autounattend.xml" -Wait
